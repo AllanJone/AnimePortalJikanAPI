795b62b7545974e580e2e313e2b4923a3026d739
Allan Jone.A <joeallan123@gmail.com>
2019-01-03 01:02:42 +0530

    Added Details View Add Details View CSS

--- AnimePortalJikanAPI/AnimePortalJikanAPI/Controllers/AnimeController.cs
+++ AnimePortalJikanAPI/AnimePortalJikanAPI/Controllers/AnimeController.cs
@@ -12,34 +12,22 @@ namespace AnimePortalJikanAPI.Controllers
 {
     public class AnimeController : Controller
     {
+
+        IJikan jikan = new Jikan(true);
+
         // GET: Anime
         public ActionResult Index()
         {
-            /*List<Anime> animeList = new List<Anime>();
-            IJikan jikan = new Jikan(true);
-            Anime anime = jikan.GetAnime(1);
-            AnimeTop topAnimeList = jikan.GetAnimeTop();
-            foreach (AnimeTopEntry animeTopEntry in topAnimeList.Top)
-            {
-                Anime animeTemp = jikan.GetAnime(animeTopEntry.MalId);
-                animeList.Add(animeTemp);
-            }
-            ViewData["animeName"] = anime.Title;*/
-
-            //List<Anime> animeList = new List<Anime>();
-            //IJikan jikan = new Jikan(true);
-            //AnimeTop topAnimeList = jikan.GetAnimeTop(1);
-            //foreach (AnimeTopEntry animeTopEntry in topAnimeList.Top)
-            //{
-            //    Anime animeTemp = jikan.GetAnime(animeTopEntry.MalId);
-            //    animeList.Add(animeTemp);
-            //}
-
             List<AnimeTopEntry> animeList = new List<AnimeTopEntry>();
-            IJikan jikan = new Jikan(true);
             AnimeTop topAnimeList = jikan.GetAnimeTop(1);
             animeList = topAnimeList.Top.ToList();
             return View(animeList);
+        }
+
+        public ActionResult Details(int id = 1)
+        {
+            Anime anime = jikan.GetAnime(id);
+            return View(anime);
         }
     }
 }
